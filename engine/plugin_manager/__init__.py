2ca4c6ee1e251ed5ae030df19948fd56b06b1169
XuChenXu <91937041+ChenXu233@users.noreply.github.com>
2026-06-20 13:08:10 +0800

    feat: add plugin dispatcher

--- engine/plugin_manager/__init__.py
+++ engine/plugin_manager/__init__.py
@@ -1,4 +1,5 @@
 # engine/plugin_manager/__init__.py
 from .base import PluginBase, PluginInfo, PluginType
+from .dispatcher import PluginDispatcher
 
-__all__ = ["PluginBase", "PluginInfo", "PluginType"]
+__all__ = ["PluginBase", "PluginInfo", "PluginType", "PluginDispatcher"]
